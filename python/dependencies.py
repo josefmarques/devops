3bd22ad7f0df9efa38598fcfb6f4e89f416ae90f
Jose Marques <jf-marques@outlook.com>
2025-07-13 16:26:49 -0300

    add gitignore

--- python/dependencies.py
+++ python/dependencies.py
@@ -1,5 +1,10 @@
+from fastapi import Depends, HTTPException
+from main import SECRET_KEY, ALGORITHM, oauth2_schema
 from models import db
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
+from models import Usuario
+from jose import jwt, JWTError
+
 
 def pegar_sessao():
     try:
@@ -8,3 +13,15 @@ def pegar_sessao():
         yield session
     finally:
         session.close()
+
+
+def verificar_token(token: str = Depends(oauth2_schema), session: Session = Depends(pegar_sessao)):
+    try:
+        dic_info = jwt.decode(token, SECRET_KEY, ALGORITHM)
+        id_usuario = int(dic_info.get("sub"))
+    except JWTError:        
+        raise HTTPException(status_code=401, detail="Acesso Negado, verifique a validade do token")
+    usuario = session.query(Usuario).filter(Usuario.id == id_usuario).first()
+    if not usuario:
+        raise HTTPException(status_code=401, detail="Acesso Inválido")
+    return usuario
