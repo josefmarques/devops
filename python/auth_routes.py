cf0525659115607783f181a33d3cc9e6c5e958c7
Jose Marques <jf-marques@outlook.com>
2025-07-13 09:01:49 -0300

    Concluindo criacao de rotas

--- python/auth_routes.py
+++ python/auth_routes.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter,Depends, HTTPException
 from models import Usuario
 from dependencies import pegar_sessao
 from main import bccrypt_context
+from schemas import UsuariosSchema
+from sqlalchemy.orm import Session
 
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
@@ -14,16 +16,16 @@ async def home():
     return {"mensagem": "Você acessou a rota padrão de autenticação", "autenticado": False}
 
 @auth_router.post("/criar_conta")
-async def criar_conta(email: str, senha: str, nome: str, session = Depends(pegar_sessao)):
-    usuario = session.query(Usuario).filter(Usuario.email==email).first()
+async def criar_conta(usuario_schema: UsuariosSchema, session: Session = Depends(pegar_sessao)):
+    usuario = session.query(Usuario).filter(Usuario.email==usuario_schema.email).first()
     if usuario:
         # já existe um usuario com esse email
-        return {"mensagem": "Já existe um usuário com esse email"}
+        raise HTTPException(status_code=400, detail="E-mail já cadastrado")
     else:
-        senha_criptografada = bccrypt_context.hash(senha)
-        novo_usuario = Usuario(nome, email, senha_criptografada)
+        senha_criptografada = bccrypt_context.hash(usuario_schema.senha)
+        novo_usuario = Usuario(usuario_schema.nome, usuario_schema.email, senha_criptografada, usuario_schema.ativo, usuario_schema.admin)
         session.add(novo_usuario)
         session.commit()
-        return {"mensagem": "Usuário cadastrado com sucesso!"}
+        return {"mensagem":f"Usuário cadastrado com sucesso! {usuario_schema.email}"}
 
     
